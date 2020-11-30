4af10add2a84789cb9b59f3456b3b008a12f00d3
Michael <michael@spinningcube.co.nz>
2020-11-30 23:40:15 +1300

    Can register but contact form not working

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,3 +1,25 @@
 <template>
-  REGISTER
+  <base-card>
+    <section>
+      <h2>Register as a coach now!</h2>
+      <coach-form @save-data="saveData"></coach-form>
+    </section>
+  </base-card>
+
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+export default {
+  components: {
+    CoachForm
+  },
+  methods: {
+    saveData(data) {
+      this.$store.dispatch('coaches/registerCoach', data);
+      this.$router.replace('/coaches');
+    }
+  }
+}
+</script>
+
